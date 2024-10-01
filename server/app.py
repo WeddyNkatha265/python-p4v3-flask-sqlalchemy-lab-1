39511373b286883cd0dab4e599d3e136e082dcad
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-10-01 12:15:30 +0300

    Updates flask sqlalchemy lab 1

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,31 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify({
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }) , 200
+    else:
+        return jsonify({'message': 'Earthquake 9999 not found.'}), 404
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return jsonify({
+        'count': len(earthquakes),
+        'quakes': [{
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        } for earthquake in earthquakes]
+    })  
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
